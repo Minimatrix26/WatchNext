75707bc48494bd20ccd08eb8be3b48f1573e73a1
Minimatrix26 <armand.brudea@yahoo.com>
2024-08-13 15:28:28 +0300

    Rebase pt a face PR

---
+++ src/main/java/com/example/demo/categories/CategoryConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo.categories;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CategoryConfig {
+
+//    @Bean
+//    CommandLineRunner otherCommandLineRunner(CategoryRepository categoryRepository) {
+//        return args -> {
+//            Category scifi = new Category("Science Fiction");
+//
+//            categoryRepository.save(scifi);
+//        };
+//    }
+}
