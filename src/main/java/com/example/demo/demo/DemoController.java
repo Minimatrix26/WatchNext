75e103c948ee481d43a7391a5a57a43bd636fe0d
Minimatrix26 <armand.brudea@yahoo.com>
2024-09-07 15:11:57 +0300

    + Movie and Category endpoints access restriction

--- src/main/java/com/example/demo/demo/DemoController.java
+++ src/main/java/com/example/demo/demo/DemoController.java
@@ -1,18 +1,49 @@
 package com.example.demo.demo;
 
+import com.example.demo.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/api/v1/demo-controller")
+@RequiredArgsConstructor
 public class DemoController {
+
+    private final UserRepository userRepository;
 
     @GetMapping
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello from secured endpoint");
     }
+
+    @GetMapping("/users/all")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<Object> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    @GetMapping("/users/single")
+    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN')")
+    public ResponseEntity<Object> getMyDetails() {
+        return ResponseEntity.ok(userRepository.findByEmail(getLoggedInUserDetails().getUsername()));
+    }
+
+    public UserDetails getLoggedInUserDetails(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null && authentication.getPrincipal() instanceof UserDetails){
+            return (UserDetails) authentication.getPrincipal();
+        }
+        return null;
+    }
+
 
 }
 
